3e70f0ef9cb14bdeb560192921ae67e536f7e920
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-06-08 05:40:33 +0800

    ⚙️ Chore: Setup CocoaPods Scaffolding

---
+++ ExamplePods/ConfigBasedModal/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ConfigBasedModal
+//
+//  Created by dominicstop@gmail.com on 06/08/2024.
+//  Copyright (c) 2024 dominicstop@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
