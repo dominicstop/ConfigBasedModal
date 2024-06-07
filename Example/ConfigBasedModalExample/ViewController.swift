c0651332b75f425f91c424bfff9932f26c4bb2dd
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-06-08 06:14:09 +0800

    🆕 Add: SPM Example Project

---
+++ Example/ConfigBasedModalExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConfigBasedModalExample
+//
+//  Created by Dominic Go on 6/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
