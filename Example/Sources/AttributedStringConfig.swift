7a9d5aa61d011e2742f1d09fb37afac8d072e7fa
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-06-09 16:48:13 +0800

    ⭐️ Impl: Ex - `AttributedStringConfig`

---
+++ Example/Sources/AttributedStringConfig.swift
@@ -0,0 +1,170 @@
+//
+//  AttributedStringConfig.swift
+//  ConfigBasedModalExample
+//
+//  Created by Dominic Go on 6/9/24.
+//
+
+import UIKit
+
+// TODO: Move to `SwiftUtilities`
+public struct AttributedStringConfig {
+
+  public static let newLine: Self = .init(text: "\n");
+  
+  //static var attributesMap: PartialKeyPath<Self>
+  
+  // MARK: Properties
+  // ----------------
+
+  public var text: String;
+  public var fontConfig: FontConfig;
+  
+  public var paragraphStyle: NSParagraphStyle;
+  public var foregroundColor: UIColor;
+  public var backgroundColor: UIColor?;
+  
+  public var strikethroughStyle: NSUnderlineStyle?;
+  public var strikethroughColor: UIColor?;
+  
+  public var underlineStyle: NSUnderlineStyle?;
+  public var underlineColor: UIColor?;
+  
+  public var strokeColor: UIColor?;
+  public var strokeWidth: CGFloat?;
+  
+  // TODO: - TBA:
+  // ligature
+  // kern
+  // tracking
+  // shadow
+  // textEffect
+  // attachment
+  // link
+  // baselineOffset
+  // writingDirection
+  
+  // MARK: - Computed Properties
+  // ---------------------------
+  
+  public var attributes: [NSAttributedString.Key: Any] {
+    var attributes: [NSAttributedString.Key: Any] = [:];
+    
+    attributes[.font] = self.fontConfig.makeFont();
+    attributes[.paragraphStyle] = self.paragraphStyle;
+    attributes[.foregroundColor] = self.foregroundColor;
+    
+    if let backgroundColor = self.backgroundColor {
+      attributes[.backgroundColor] = backgroundColor;
+    };
+    
+    if let strikethroughStyle = self.strikethroughStyle {
+      attributes[.strikethroughStyle] = strikethroughStyle;
+    };
+    
+    if let strikethroughColor = self.strikethroughColor {
+      attributes[.strikethroughColor] = strikethroughColor;
+    };
+    
+    if let underlineStyle = self.underlineStyle {
+      attributes[.underlineStyle] = underlineStyle;
+    };
+    
+    if let underlineColor = self.underlineColor {
+      attributes[.underlineColor] = underlineColor;
+    };
+    
+    if let strokeColor = self.strokeColor {
+      attributes[.strokeColor] = strokeColor;
+    };
+    
+    if let strokeWidth = self.strokeWidth {
+      attributes[.strokeWidth] = strokeWidth;
+    };
+    
+    return attributes;
+  };
+  
+  // MARK: Init
+  // ----------
+  
+  public init(
+    text: String,
+    fontConfig: FontConfig = .default,
+    paragraphStyle: NSParagraphStyle = .default,
+    color: UIColor = .systemBackground,
+    backgroundColor: UIColor? = nil,
+    strikethroughStyle: NSUnderlineStyle? = nil,
+    strikethroughColor: UIColor? = nil,
+    underlineStyle: NSUnderlineStyle? = nil,
+    underlineColor: UIColor? = nil,
+    strokeColor: UIColor? = nil,
+    strokeWidth: CGFloat? = nil
+  ) {
+  
+    self.text = text;
+    self.fontConfig = fontConfig;
+    self.paragraphStyle = paragraphStyle;
+    self.foregroundColor = color;
+    self.backgroundColor = backgroundColor;
+    self.strikethroughStyle = strikethroughStyle;
+    self.strikethroughColor = strikethroughColor;
+    self.underlineStyle = underlineStyle;
+    self.underlineColor = underlineColor;
+    self.strokeColor = strokeColor;
+    self.strokeWidth = strokeWidth;
+  };
+  
+  public init(
+    text: String,
+    size: CGFloat,
+    weight: UIFont.Weight = .regular,
+    isBold: Bool = false,
+    isItalic: Bool = false,
+    width: FontConfig.FontWidth = .default,
+    paragraphStyle: NSParagraphStyle = .default,
+    color: UIColor = .systemBackground,
+    backgroundColor: UIColor? = nil,
+    strikethroughStyle: NSUnderlineStyle? = nil,
+    strikethroughColor: UIColor? = nil,
+    underlineStyle: NSUnderlineStyle? = nil,
+    underlineColor: UIColor? = nil,
+    strokeColor: UIColor? = nil,
+    strokeWidth: CGFloat? = nil
+  ) {
+  
+    let fontConfig = FontConfig(
+      size: size,
+      weight: weight,
+      isBold: isBold,
+      isItalic: isItalic,
+      width: width
+    );
+    
+    self.init(
+      text: text,
+      fontConfig: fontConfig,
+      paragraphStyle: paragraphStyle,
+      color: color,
+      backgroundColor: backgroundColor,
+      strikethroughStyle: strikethroughStyle,
+      strikethroughColor: strikethroughColor,
+      underlineStyle: underlineStyle,
+      underlineColor: underlineColor,
+      strokeColor: strokeColor,
+      strokeWidth: strokeWidth
+    );
+  };
+  
+  // MARK: - Functions
+  // -----------------
+  
+  func makeAttributedString() -> NSMutableAttributedString {
+    return .init(
+      string: self.text,
+      attributes: self.attributes
+    );
+  };
+};
+
+
