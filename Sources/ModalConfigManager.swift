e3a92fe8a15eb01c9db0b95d0dea7834a64678a2
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-06-13 12:11:14 +0800

    ⭐️ Impl: `ModalConfigManager`

---
+++ Sources/ModalConfigManager.swift
@@ -0,0 +1,78 @@
+//
+//  ModalConfigManager.swift
+//
+//
+//  Created by Dominic Go on 6/10/24.
+//
+
+import UIKit
+import DGSwiftUtilities
+
+
+public class ModalConfigManager {
+
+  // MARK: - Properties
+  // ------------------
+
+  var _presentingVC: UIViewController? = nil;
+  
+  // MARK: - Computed Properties
+  // ---------------------------
+  
+  public var window: UIWindow? {
+    if let presentingVC = self._presentingVC,
+       let window = presentingVC.view.window {
+       
+      return window;
+    };
+    
+    if let activeWindow = UIApplication.shared.activeWindow {
+      return activeWindow;
+    };
+    
+    return UIApplication.shared.allWindows.first {
+      $0.isKeyWindow;
+    };
+  };
+  
+  public var presentingVC: UIViewController? {
+    if let presentingVC = self._presentingVC {
+      return presentingVC;
+    };
+    
+    guard let window = self.window else {
+      return nil;
+    };
+    
+    return window.topmostPresentedViewController;
+  };
+  
+  // MARK: - Init
+  // ------------
+  
+  public init(
+    presentingViewController presentingVC: UIViewController? = nil
+  ){
+    self._presentingVC = presentingVC;
+  };
+  
+  // MARK: - Methods
+  // ---------------
+  
+  public func presentModal(
+    viewControllerToPresent presentedVC: UIViewController,
+    presentingViewController presentingVC: UIViewController? = nil
+  ){
+    guard let presentingVC = presentingVC ?? self.presentingVC else {
+      return;
+    };
+  
+    presentingVC.present(
+      presentedVC,
+      animated: true,
+      completion: {
+        // no-op
+      }
+    );
+  };
+};
